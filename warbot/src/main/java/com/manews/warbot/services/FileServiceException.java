5d37ca6016b9adf8c5191755ec169478af544f50
BuildTools <unconfigured@null.spigotmc.org>
2019-10-08 19:23:50 +0200

    Deleted duplicated src

--- warbot/src/main/java/com/manews/warbot/services/FileServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.manews.warbot.services;
-
-public class FileServiceException extends Exception {
-    public FileServiceException(String message) {
-        super(message);
-    }
-}
--- warbot/src/main/java/com/manews/warbot/services/KillServiceException.java
+++
@@ -1,7 +0,0 @@
-package com.manews.warbot.services;
-
-public class KillServiceException extends Exception {
-    public KillServiceException(String message) {
-        super(message);
-    }
-}
